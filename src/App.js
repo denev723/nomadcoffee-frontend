db2edb3113e5f05c2b10da74550825e2fbae7de6
denev <denev723@gmail.com>
2023-11-08 02:39:09 +0900

    Challenge Admin Login

--- src/App.js
+++ src/App.js
@@ -1,29 +1,43 @@
-import { useReactiveVar } from "@apollo/client";
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import { ApolloProvider, useReactiveVar } from "@apollo/client";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./screens/Home";
 import Login from "./screens/Login";
 import NotFound from "./screens/NotFound";
-import { darkModeVar, isLoggedInVar } from "./apollo";
+import { client, darkModeVar, isLoggedInVar } from "./apollo";
 import { ThemeProvider } from "styled-components";
 import { GlobalStyles, darkTheme, lightTheme } from "./styles";
+import { HelmetProvider } from "react-helmet-async";
+import routes from "./routes";
+import SignUp from "./screens/SignUp";
 
 function App() {
   const isLoggedIn = useReactiveVar(isLoggedInVar);
   const darkMode = useReactiveVar(darkModeVar);
   return (
-    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
-      <GlobalStyles />
-      <Router>
-        <Switch>
-          <Route path="/" exact>
-            {isLoggedIn ? <Home /> : <Login />}
-          </Route>
-          <Route>
-            <NotFound />
-          </Route>
-        </Switch>
-      </Router>
-    </ThemeProvider>
+    <ApolloProvider client={client}>
+      <HelmetProvider>
+        <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+          <GlobalStyles />
+          <Router>
+            <Switch>
+              <>
+                <Route path={routes.home} exact>
+                  {isLoggedIn ? <Home /> : <Login />}
+                </Route>
+                {!isLoggedIn ? (
+                  <Route path={routes.signUp}>
+                    <SignUp />
+                  </Route>
+                ) : null}
+                <Route>
+                  <NotFound />
+                </Route>
+              </>
+            </Switch>
+          </Router>
+        </ThemeProvider>
+      </HelmetProvider>
+    </ApolloProvider>
   );
 }
 
---
+++ src/components/auth/AuthLayout.js
@@ -0,0 +1,42 @@
+import { useReactiveVar } from "@apollo/client";
+import styled from "styled-components";
+import { darkModeVar } from "../../apollo";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMoon, faSun } from "@fortawesome/free-regular-svg-icons";
+
+const Container = styled.div`
+  display: flex;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const Wrapper = styled.div`
+  max-width: 350px;
+  width: 100%;
+`;
+
+const Footer = styled.footer`
+  margin-top: 20px;
+`;
+
+const DarkModeBtn = styled.span`
+  cursor: pointer;
+`;
+
+function AuthLayout({ children }) {
+  const darkMode = useReactiveVar(darkModeVar);
+  return (
+    <Container>
+      <Wrapper>{children}</Wrapper>
+      <Footer>
+        <DarkModeBtn>
+          <FontAwesomeIcon icon={darkMode ? faSun : faMoon} />
+        </DarkModeBtn>
+      </Footer>
+    </Container>
+  );
+}
+
+export default AuthLayout;
