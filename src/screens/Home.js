ad7ddabbb4ea6239edae5f1e865f977f90fe8c73
denev <denev723@gmail.com>
2023-11-09 23:40:09 +0900

    Add Shop - failed

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,11 +1,44 @@
 import { isLoggedInVar } from "../apollo";
+import { gql, useQuery } from "@apollo/client";
+import styled from "styled-components";
+import { logUserOut } from "../apollo";
+
+const Container = styled.div``;
+
+const Header = styled.div``;
+
+const SEE_COFFEESHOPS_QUERY = gql`
+  query coffeeShops($lastId: Int) {
+    seeCoffeeShops(lastId: $lastId) {
+      id
+      name
+      longitude
+      latitude
+      categories {
+        name
+      }
+      photos {
+        url
+      }
+    }
+  }
+`;
 
 function Home() {
+  const { loading, error, data } = useQuery(SEE_COFFEESHOPS_QUERY);
   return (
-    <div>
-      <h1>Home</h1>
-      <button onClick={() => isLoggedInVar(false)}>Log out now!</button>
-    </div>
+    <Container>
+      <Header>
+        <button onClick={logUserOut}>Logout</button>
+      </Header>
+      <ul>
+        {data?.seeCoffeeShops?.map((coffeeShop) => (
+          <li key={coffeeShop.id}>
+            <p>{coffeeShop.name}</p>
+          </li>
+        ))}
+      </ul>
+    </Container>
   );
 }
 
